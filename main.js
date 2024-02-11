cefd47ec6de6fefa4437696db88d8c210750989a
lalahabi <159227690+lalahabi@users.noreply.github.com>
2024-02-11 09:40:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,67 @@
+let computerNum = 0;
+let playButton = document.getElementById("play-button");
+let userInput = document.getElementById("user-input");
+let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5
+let gameOver = false 
+let chanceArea = document.getElementById("chance-area")
+let history = []
 
-let computerNum = 0
-let playButton = document.getElementById("play-button")
-let userInput = document.getElementById("user-input")
-let resultArea = document.getElementById("result-area")
-console.log(playButton)
+console.log(playButton);
 
-playButton.addEventListener("click",play)
+playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus",function(){userInput.value=""});
 
-function pickRandomNum(){
-   computerNum = Math.floor(Math.random()*100)+1;
-   console.log("정답, computerNum")
+function pickRandomNum() {
+  computerNum = Math.floor(Math.random() * 100) + 1;
+  console.log("Correct, computerNum");
 }
 
-function play(){
-   let userValue = userInput.value;
-   if(userValue < computerNum){
-      resultArea.textContent = "UP!!!"
-   }else if(userValue > computerNum){
-      resultArea.textContent = "Down!!!"
-   }else{
-      resultArea.textContent = "맞추셨습니다!!!"
-   }
+function play() {
+  let userValue = userInput.value;
+
+  if(userValue<1 || userValue>100){
+   resultArea.textContent="Enter your guess from 1 to 100"
+   return;
+  }
+
+  if(history.includes(userValue)){
+   resultArea.textContent="You've already entered the number, please use another number."
+   return;
+  }
+
+  chances --;
+  chanceArea.textContent=`You get : ${chances} chances left`
+  console.log("chance", chances)
+  if (userValue < computerNum) {
+    resultArea.textContent = "My number is greater than  6";
+  } else if (userValue > computerNum) {
+    resultArea.textContent = "My number is less than 90";
+  } else {
+    resultArea.textContent = "Well Done!";
+    gameOver = true
+  }
+
+  history.push(userValue)
+  console.log(history)
+
+
+  if(chances < 1){
+   gameOver  = true
+  }
+
+  if(gameOver == true){
+   playButton.disabled = true
+  }
 }
+
+function reset() {
+userInput.value = ""
+pickRandomNum();
+
+resultArea.textContent = "결과값이 여기 나옵니다."
+}
+
 pickRandomNum();
