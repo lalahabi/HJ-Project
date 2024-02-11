43b991d6a92dab9f5f32837bcde3d7063a12fb4f
lalahabi <159227690+lalahabi@users.noreply.github.com>
2024-02-10 18:16:49 -0800

    Update main.js

--- main.js
+++ main.js
@@ -36,9 +36,9 @@ function play() {
   chanceArea.textContent=`You get : ${chances} chances left`
   console.log("chance", chances)
   if (userValue < computerNum) {
-    resultArea.textContent = "My number is greater than" + userValue;
+    resultArea.textContent = "My number is greater than " + userValue;
   } else if (userValue > computerNum) {
-    resultArea.textContent = "My number is less than" + userValue;
+    resultArea.textContent = "My number is less than " + userValue;
   } else {
     resultArea.textContent = "Well Done!";
     gameOver = true
